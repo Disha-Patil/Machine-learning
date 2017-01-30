be3c29d8f170c6e79651d27220386f78ec1e230e
Disha-Patil <dishapatil10@yahoo.com>
2017-01-30 22:17:22 +0530

    Update naive_bayes_spam-filter.r

--- naive_bayes_spam-filter.r
+++ naive_bayes_spam-filter.r
@@ -1,8 +1,19 @@
 #data for training
 library(kernlab)
-random<-spam[sample(1:nrow(spam)),]
+random<-spam[sample(2:nrow(spam)),]
 random[,ncol(spam)]<-as.numeric(random[,ncol(spam)])
 #non spam == 1 and spam == 2
+
+folds<-10
+fold_col<-(ncol(random) + 1)
+random[,fold_col] <- rep(1:folds,each=(nrow(random)/folds))
+
+folds<-function(fold,data)
+{ 
+train_set <- data[-(which(data[,fold_col] == fold)),1:ncol(spam)]
+test_set <- data[which(data[,fold_col] == fold),1:ncol(spam)]
+return(list(train_set=train_set,test_set=test_set))
+}
 
 
 #find the mu and sigma of training data
@@ -34,7 +45,7 @@ classification<-function(example)
           p<-c()
             for (j in 1:2)
             {
-              p[j]<-posterior_prob(example,class_prob,training(random[which(random[,ncol(spam)]==j),-ncol(spam)]))
+              p[j]<-posterior_prob(example,class_prob,training(train_data[which(train_data[,ncol(spam)]==j),1:57]))
             }
 
           predict_class<-which(p==max(p))
@@ -44,14 +55,30 @@ classification<-function(example)
 	example<-cbind(example,predict_class)
 
 #print example with predicted class
-return(print(example))
+#return(print(example))
+return(predict_class)
 }
 
 
 
 #e is any example 
 
-e<-spam[4000,]
-classification(e)
+
+acc<-c()
+for(j in 1:10)
+{
+folds_data<-folds(j,random[,])
+test<-folds_data$test_set
+train<-folds_data$train_set
+predicted_class<-c()
+for(i in 1:nrow(test))
+{
+predicted_class[i]<-classification(test[i,],train)
+}
+
+acc[j]<-(length(which(test[,ncol(spam)]==predicted_class)))/nrow(test)
+}
+print(mean(acc))
+
 
 
